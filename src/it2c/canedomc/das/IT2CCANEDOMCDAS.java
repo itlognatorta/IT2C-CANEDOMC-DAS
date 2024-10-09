f15f7a2cdc1b13a285bc8ccd9318ae4ff0ee37a2
ROMOXD <mrkvnurm80@gmail.com>
2024-10-09 14:39:25 +0800

    DELETED IS DONE

--- src/it2c/canedomc/das/IT2CCANEDOMCDAS.java
+++ src/it2c/canedomc/das/IT2CCANEDOMCDAS.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 
 public class IT2CCANEDOMCDAS {
     
+  
+    
     public void addCustomers(){
         Scanner sc = new Scanner(System.in);
         config conf = new config(); 
@@ -24,7 +26,55 @@ public class IT2CCANEDOMCDAS {
         conf.addCustomers(sql, fname, lname, email, contact, address);
 
     }
+    
+    private void viewCustomers() {
+        String Cmquery = "SELECT * FROM customer";
+        String[] hdrs = {"ID", "Firstname", "Lastname", "Email", "Contact Number", "Address"};
+        String[] colmns = {"id", "fname", "lname", "email", "Contactnum", "address"};
+        config conf = new config();
+        conf.viewCustomer(Cmquery, hdrs, colmns);
+    }
+    
+    private void updateCustomers(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter customer ID: ");
+        int id = sc.nextInt();
         
+        System.out.println("Enter new Customer First Name:");
+        String ufname = sc.next();
+        
+        System.out.println("Enter new Customer Last Name:");
+        String ulname = sc.next();
+        
+        System.out.println("Enter new Customer Email:");
+        String uemail = sc.next();
+        
+        System.out.println("Enter new Customer Contact Number:");
+        String ucontact = sc.next();
+        
+        System.out.println("Enter new Customer Address:");
+        String uaddress = sc.next();
+        
+        String sql = "UPDATE customer SET fname = ?, lname = ?, email = ?, Contactnum = ?, address = ? WHERE id = ?";
+        
+        config conf = new config();
+        conf.updateCustomer(sql, ufname, ulname, uemail, ucontact, uaddress, id);
+        
+        
+    }
+
+    private void deleteCustomers() {      
+         Scanner sc = new Scanner(System.in);        
+         System.out.print("Enter customer ID: ");
+         int id = sc.nextInt();
+
+        String sqlDelete = "DELETE FROM customer WHERE id = ?";
+        
+        config conf = new config();
+        conf.deleteRecords(sqlDelete, id);
+    }
+
+
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
@@ -41,26 +91,43 @@ public class IT2CCANEDOMCDAS {
             System.out.print("Enter Action: ");
             int action = sc.nextInt();
             
+            IT2CCANEDOMCDAS das = new IT2CCANEDOMCDAS();
             switch(action){
+                
                 
                 case 1:
                     
-                    IT2CCANEDOMCDAS das = new IT2CCANEDOMCDAS();
+                    
                     das.addCustomers();
                     
                     break;
+                    
+                case 2:
+                   
+                    das.viewCustomers();                 
+                    
+                    break;
+                    
+                case 3:
+                    
+                    das.viewCustomers();   
+                    das.updateCustomers();
+                    break;
+            
+                case 4: 
+                    das.viewCustomers();
+                    das.deleteCustomers();
+                    das.viewCustomers();
+                    break;
             }
             
-            
+          
             System.out.println("Do you want to continue? (yes/no): ");
-            response = sc.nextLine();
-        }while(response.equalsIgnoreCase("yes"));
+            response = sc.next();
+            
+        }while(response.equals("yes"));
         System.out.println("Thank You, See you again!");
-        
-
-
-
-        
+             
         
      
     }
